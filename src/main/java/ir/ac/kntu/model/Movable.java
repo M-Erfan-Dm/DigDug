da8c33f0c2ebc567e01ff3620f2bc65ac25314ed
M-Erfan-Dm <erfan.dtm@gmail.com>
2021-06-27 18:20:28 +0430

    getNextPositionByStep in Movable

--- src/main/java/ir/ac/kntu/model/Movable.java
+++ src/main/java/ir/ac/kntu/model/Movable.java
@@ -1,5 +1,9 @@
 package ir.ac.kntu.model;
 
 public interface Movable {
-    void move(int x,int y);
+    void move(int gridX,int gridY);
+
+    static double getNextPositionByStep(double curPos, double finalPos, double step){
+        return curPos + Math.signum(finalPos - curPos) * step;
+    }
 }

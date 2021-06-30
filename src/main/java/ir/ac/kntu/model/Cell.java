1eb15e4a504c01c95184b018957b2518de77871e
M-Erfan-Dm <erfan.dtm@gmail.com>
2021-06-30 20:02:20 +0430

    add getFirstObjectByType in Cell

--- src/main/java/ir/ac/kntu/model/Cell.java
+++ src/main/java/ir/ac/kntu/model/Cell.java
@@ -37,14 +37,22 @@ public class Cell {
         gameObjects.remove(gameObject);
     }
 
-    public <T extends GameObject> List<T> getObjectsByType(Class<T> gameObjectType) {
+    public <T extends GameObject> List<T> getAllObjectsByType(Class<T> gameObjectType) {
         return gameObjects.stream().filter(gameObjectType::isInstance)
                 .map(gameObjectType::cast).collect(Collectors.toList());
     }
 
     public <T extends GameObject> boolean hasObjectType(Class<T> gameObjectType){
-        List<T> objects = getObjectsByType(gameObjectType);
+        List<T> objects = getAllObjectsByType(gameObjectType);
         return !objects.isEmpty();
+    }
+
+    public <T extends GameObject> T getFirstObjectByType(Class<T> gameObjectType){
+        List<T> objects = getAllObjectsByType(gameObjectType);
+        if (!objects.isEmpty()){
+            return objects.get(0);
+        }
+        return null;
     }
 
     public List<GameObject> getGameObjects() {
